3a273a587fa3812386869218b5a99bbcfc31d05b
S Joel Franklin <53593501+Joelfranklin96@users.noreply.github.com>
2026-09-11 06:58:33 -0500

    Step 2: apply_temperature

--- model.py
+++ model.py
@@ -14,8 +14,11 @@ def stable_softmax(logits):
     temp = np.sum(logits, axis=-1, keepdims=True)
     return logits/temp
 
-# Step 2 - apply_temperature (not yet solved)
-# TODO: implement
+# Step 2 - apply_temperature
+def apply_temperature(logits, temperature):
+    if temperature > 0:
+        return logits/temperature
+    return logits
 
 # Step 3 - top_k_filter (not yet solved)
 # TODO: implement
